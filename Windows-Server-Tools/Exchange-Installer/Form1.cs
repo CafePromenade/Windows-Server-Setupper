f88d22ca45aa5eb958abd8099a27b01cab76edf1
CafePromenade <153756308+CafePromenade@users.noreply.github.com>
2024-11-27 22:23:16 -0500

    r

--- Windows-Server-Tools/Exchange-Installer/Form1.cs
+++ Windows-Server-Tools/Exchange-Installer/Form1.cs
@@ -242,6 +242,9 @@ namespace Exchange_Installer
                 Command.RunCommandHidden("schtasks /delete /tn \"" + "Run EXCHANGE\" /f");
                 await Functions.RunPowerShellScript("Get-Service | Where-Object {$_.DisplayName -like \"*Exchange*\"} | ForEach-Object {\r\n    Set-Service -Name $_.Name -StartupType Automatic\r\n}\r\n");
                 await Functions.RunPowerShellScript("Get-Service | Where-Object {$_.DisplayName -like \"*Exchange*\" -and $_.Status -ne \"Running\"} | ForEach-Object {\r\n    Start-Service -Name $_.Name\r\n}\r\n");
+                await Functions.RunPowerShellScript("iisreset");
+                await Functions.RunPowerShellScript("iisreset");
+                await Functions.RunPowerShellScript("Restart-Service MSExchange* -Force");
                 //await Command.RunCommand("iisreset");
                 try
                 {
