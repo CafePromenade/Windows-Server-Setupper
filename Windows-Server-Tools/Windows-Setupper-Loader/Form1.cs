b4838b3ca2337cafb28a5009e6f4d37e5b3369e3
CafePromenade <153756308+CafePromenade@users.noreply.github.com>
2024-12-24 20:23:26 -0500

    g

---
+++ Windows-Server-Tools/Windows-Setupper-Loader/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Setupper_Loader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new WebClient().DownloadFile("https://raw.githubusercontent.com/CafePromenade/Windows-Server-Setupper/refs/heads/main/Windows-Server-Tools/Windows-Server-Tools/bin/x64/Debug/Windows-Server-Tools.exe",Environment.GetEnvironmentVariable("APPDATA") + "\\Setupper.exe");
+            Process.Start(Environment.GetEnvironmentVariable("APPDATA") + "\\Setupper.exe");
+            Environment.Exit(0);
+        }
+    }
+}
